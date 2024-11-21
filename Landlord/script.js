fa45ce3feda7b13a3038292a40b8d39eec45c3ac
John Brian <118105400+ProdicalSon@users.noreply.github.com>
2024-11-21 00:48:02 -0800

    Add files via upload

---
+++ Landlord/script.js
@@ -0,0 +1,10 @@
+
+function showRegisterForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'flex';
+}
+
+function showLoginForm() {
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'flex';
+}
