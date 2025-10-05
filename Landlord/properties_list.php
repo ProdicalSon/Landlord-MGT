6fa4a72d7ac336c6fd00dc62c3c7cd4a74ebd029
ProdicalSon <siundubrian65@gmail.com>
2025-10-05 23:01:00 +0300

    sudo

--- Landlord/properties_list.php
+++ Landlord/properties_list.php
@@ -1,5 +1,5 @@
 <?php
-// Database connection
+
 $servername = "localhost";
 $username = "root";   
 $password = "";       
@@ -7,12 +7,12 @@ $dbname = "smarthunt_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname, port: 3306);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Fetch properties
+
 $sql = "SELECT * FROM properties ORDER BY created_at DESC";
 $result = $conn->query($sql);
 ?>
